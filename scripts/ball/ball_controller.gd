cea5fc33fbe4dc1dfb9d85bdaf34e65d1be22f90
Spider987 <adpkemp326@gmail.com>
2025-07-07 20:31:24 -0400

    Added collision system to ball. Added left and right hitboxes.

--- scripts/ball/ball_controller.gd
+++ scripts/ball/ball_controller.gd
@@ -1,10 +1,31 @@
 extends CharacterBody2D
 
-
-
-func _determine_direction():
-	return
+var collision
+var started = false
+var _velocity = Vector2(-1,.2) * 300
 
 func _physics_process(delta):
-	velocity = Vector2(-1,0)
-	move_and_collide(velocity * delta * 20)
+	print(_velocity)
+	collision = move_and_collide(_velocity * delta)
+	#Conditionals to handle where the ball should be going. Final if statement handles inital direction.
+	if collision:
+		#velocity = Vector2(-velocity.x, velocity.y + collision.get_collider_velocity().y)
+		_velocity = _velocity.bounce(collision.get_normal()) * 1.01 + collision.get_collider_velocity()
+		if _velocity.length() > 2000:
+			_velocity = _velocity.normalized() * 2000 
+		move_and_collide(_velocity * delta)
+		#print(collision.get_collider())
+	
+	#if collision_count == 0:
+		#velocity = Vector2(-1,0)
+		#move_and_collide(velocity * delta * 20)
+		#collision_count = 1
+	#if collision_count == 1:
+		#velocity = Vector2(1,0)
+		#move_and_collide(velocity * delta * 20)
+		#collision_count = 0
+
+	if started == false:
+		move_and_collide(_velocity * delta)
+		started = true
+	
--- scripts/collision/collision_handler.gd
+++ scripts/collision/collision_handler.gd
@@ -1,6 +1,1 @@
-extends Area2D
-
-
-func _on_area_entered(area):
-	if area.name.begins_with == "Paddle":
-		print("Im working as intended")
+extends CharacterBody2D
